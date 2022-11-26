96f759f5386f33361a31e82d3d46ddf1add00a04
xlearns <2447684095@qq.com>
2022-11-26 21:07:29 +0800

    feat: eslint prettier and husky init

---
+++ bin/templates/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  extends: [],
+  overrides: [],
+  settings: {},
+  rules: {},
+};
